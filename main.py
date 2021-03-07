41c307100737ab2935f6258db07d84d59b8f9ffb
chaowen guo <chaowen.guo1@gmail.com>
2021-03-07 14:53:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ tensorflow.config.experimental_connect_to_cluster(resolver)
 # This is the TPU initialization code that has to be at the beginning.
 tensorflow.tpu.experimental.initialize_tpu_system(resolver)
 print("All devices: ", tensorflow.config.list_logical_devices('TPU'))
-
-tpu = tensorflow.distribute.cluster_resolver.TPUClusterResolver()
-tensorflow.config.experimental_connect_to_cluster(tpu)
-tensorflow.tpu.experimental.initialize_tpu_system(tpu)
-tpu_strategy = tensorflow.distribute.TPUStrategy(tpu)
 mnist = tensorflow.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255, x_test / 255
