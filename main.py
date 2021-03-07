99634912b78197c73aced5006fa39d44159ccd13
chaowen guo <chaowen.guo1@gmail.com>
2021-03-07 14:15:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ x_train, x_test = x_train / 255, x_test / 255
 model = tensorflow.keras.models.Sequential([tensorflow.keras.layers.Flatten(input_shape=(28, 28)), tensorflow.keras.layers.Dense(128, activation='relu'), tensorflow.keras.layers.Dropout(0.2), tensorflow.keras.layers.Dense(10, activation='softmax')])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=5)
+model.save('model')
 model.evaluate(x_test, y_test, verbose=2)
